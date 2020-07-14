c4d808e3a19cad565809353bbf139ae1e318d703
GooseJuus <gustavosixsam@gmail.com>
2020-07-14 13:28:49 -0400

    Pushushing PGN files

--- process_data.py
+++ process_data.py
@@ -162,10 +162,10 @@ def gen_player_data(infile, outfile, player_name):
 	
 
 def main():
-	datafile = '/Users/nryken/Desktop/ProjectModel/ficsgamesdb_search_143934.pgn'
-	playerfile = '/Users/nryken/Desktop/ProjectModel/ficsgamesdb_thomass_2020_nomovetimes'
-	gen_board_pair_data(datafile, '/Users/nryken/Desktop/ProjectModel/training_data.hdf5')
-	gen_player_data(playerfile, '/Users/nryken/Desktop/ProjectModel/player_data', 'Tal')
+	datafile = 'TheHunchosDunchos\ficsgamesdb_202001_standard_nomovetimes_143935.pgn'
+	playerfile = 'TheHunchosDunchos\Adams.pgn'
+	gen_board_pair_data(datafile, 'TheHunchosDunchos\training_data.hdf5')
+	gen_player_data(playerfile, 'TheHunchosDunchos\player_data', 'Adams')
 
 if __name__ == '__main__':
 	main()		
